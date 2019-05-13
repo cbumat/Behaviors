6af5872ccee15ee8b618b425c687e44baf78befe
cbumat <47721645+cbumat@users.noreply.github.com>
2019-05-14 01:06:14 +0200

    Add files via upload

---
+++ EmailBehavior.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Behaviors.Behaviors
+{
+    public class EmailBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += Bindable_TextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        public static bool OK { get; set; }
+
+        private void Bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Entry caja = sender as Entry;
+            String mail = caja.Text;
+            int ultimopunto = mail.LastIndexOf(".");
+            String dominio = mail.Substring(ultimopunto + 1);
+            if (caja != null)
+            {
+                try
+                {
+                    if (mail.Contains("@") == false)
+                    {
+                        caja.TextColor = Color.Red;
+                        OK = false;
+                    }
+                    else if (mail.StartsWith("@") || mail.EndsWith("@"))
+                    {
+                        caja.TextColor = Color.Red;
+                        OK = false;
+                    }
+                    else if (mail.IndexOf("@") != mail.LastIndexOf("@"))
+                    {
+                        caja.TextColor = Color.Red;
+                        OK = false;
+                    }
+                    else if (mail.IndexOf(".") == -1)
+                    {
+                        caja.TextColor = Color.Red;
+                        OK = false;
+                    }
+                    else if (mail.LastIndexOf(".") < mail.IndexOf("@"))
+                    {
+                        caja.TextColor = Color.Red;
+                        OK = false;
+                    }
+                    else if (dominio.Length <= 2 || dominio.Length >= 4)
+                    {
+                        caja.TextColor = Color.Red;
+                        OK = false;
+                    }
+                    else
+                    {
+                        caja.TextColor = Color.Green;
+                        OK = true;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= Bindable_TextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
---
+++ ValidateEmail.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Behaviors.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Behaviors
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ValidateEmail : ContentPage
+	{
+		public ValidateEmail ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (EmailBehavior.OK == true)
+            {
+                this.texto.Text = "El email es válido";
+            }
+            else
+            {
+                this.texto.Text = "El email no es válido";
+            }
+        }
+    }
+}
